e3441c09c22beb9a390fbedaaab9b6dd254ff954
Andrey Makarov <viva.la.akam@gmail.com>
2022-06-05 11:20:38 +0300

    version

--- python/setup.py
+++ python/setup.py
@@ -1,9 +1,9 @@
 from setuptools import setup, find_packages
 from setuptools_rust import RustExtension
 
-VERSION = '0.0.7'
-DESCRIPTION = 'My first Python package'
-LONG_DESCRIPTION = 'My first Python package with a slightly longer description'
+VERSION = "0.0.7"
+DESCRIPTION = "My first Python package"
+LONG_DESCRIPTION = "My first Python package with a slightly longer description"
 
 # Setting up
 setup(
@@ -16,6 +16,6 @@ setup(
     packages=find_packages(),
     rust_extensions=[RustExtension("new_york_calculate.new_york_calculate", debug=False)],
     install_requires=["numpy"],
-    keywords=['python', 'first package'],
+    keywords=["python", 'first package'],
     classifiers=[]
 )
